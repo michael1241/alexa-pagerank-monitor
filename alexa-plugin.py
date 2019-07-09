95276d29e98cb967b4ba4b449d0dafa21367d96b
michael1241 <michael@jamcupboard.co.uk>
2019-07-09 18:16:16 +0100

    Update to match new alexa page format.

--- alexa-plugin.py
+++ alexa-plugin.py
@@ -6,13 +6,15 @@ import re
 
 sites = ["lichess.org", "chess24.com", "chessbomb.com", "chess.com", "chesstempo.com", "chessable.com", "chessgames.com", "blitztactics.com"]
 
-xpath = '''//*[@id="traffic-rank-content"]/div/span[2]/div[1]/span/span/div/strong/text()'''
+xpath = '''/html/body/div[1]/div/section/div/section/div[1]/div[2]/div[3]/div/div[1]/section[2]/div[3]/div[2]/div[3]/div[2]/div[1]'''
+#old xpath '''//*[@id="traffic-rank-content"]/div/span[2]/div[1]/span/span/div/strong/text()'''
+
 alexapage = "https://www.alexa.com/siteinfo/"
 
 output = []
 for site in sites:
-	pageContent=requests.get(alexapage + site)
-	tree = html.fromstring(pageContent.content)
-	rank = re.sub("\D", "", str(tree.xpath(xpath)))
-	output.append(f"popularity,website={site} rank={rank}")
+    pageContent=requests.get(alexapage + site)
+    tree = html.fromstring(pageContent.content)
+    rank = re.sub("\D", "", str(html.tostring(tree.xpath(xpath)[0])))
+    output.append(f"popularity,website={site} rank={rank}")
 print("\n".join(output))
